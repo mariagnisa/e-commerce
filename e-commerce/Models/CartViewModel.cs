ac43a936a9f8afe4bd226d31919c303c68cd7067
Maria Gustafsson Niså <maria.gnisa@gmail.com>
2017-03-24 13:32:29 +0100

    fixed cartmodel

--- e-commerce/Models/CartViewModel.cs
+++ e-commerce/Models/CartViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using e_commerce.Models;
 
 namespace e_commerce.Models
 {
@@ -10,5 +11,10 @@ namespace e_commerce.Models
         public string CartId { get; set; }
         public int ProductId { get; set; }
         public int Quantity { get; set; }    
+
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public int ProductPrice { get; set; }
+        public string ImgPath { get; set; }
     }
 }
