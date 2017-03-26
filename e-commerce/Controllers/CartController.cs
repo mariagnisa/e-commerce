95ac189930fe50852df8dda3ce9accefd168a864
Maria Gustafsson Niså <maria.gnisa@gmail.com>
2017-03-26 17:53:52 +0200

    added delete/change quantity

--- e-commerce/Controllers/CartController.cs
+++ e-commerce/Controllers/CartController.cs
@@ -25,12 +25,15 @@ namespace e_commerce.Controllers
         public ActionResult ShowCart()
         {
             List<CartViewModel> Cart;
-            var CartId = Request.Cookies["shoppingCart"].Value;
-
-            if (CartId == null)
+            var CartCookie = Request.Cookies["shoppingCart"];
+            
+            if (CartCookie == null)
             {
+                ViewBag.Message = "Your cart is currently empty.";
+                return View();
+            }
 
-            } 
+            var CartId = CartCookie.Value;
 
             using (var connection = new SqlConnection(this.connectionString))
             {
@@ -48,14 +51,14 @@ namespace e_commerce.Controllers
         {
             string CartId;
 
-            //See if there are any cookie, if not create one
+            //See if there is any cookie, if not create one
             if (Request.Cookies["shoppingCart"] == null)
             {
-
+                //Give the cookie an unique id
                 CartId = Guid.NewGuid().ToString();
                 HttpCookie Cookie = new HttpCookie("shoppingCart");
                 Cookie.Value = CartId;
-                Cookie.Expires = DateTime.Now.AddDays(30d);
+                Cookie.Expires = DateTime.Now.AddDays(2d);
                 Response.Cookies.Add(Cookie);  
 
             } else
@@ -63,14 +66,52 @@ namespace e_commerce.Controllers
                 CartId = Request.Cookies["shoppingCart"].Value;
             }
 
+            //If product already exists in db, update quantity otherwise add product to cart
             using (var connection = new SqlConnection(this.connectionString))
             {
-                var insert = "insert into Cart (CartId, ProductId, Quantity) values (@CartId, @ProductId, @Quantity)";
-                var parameters = new { CartId = CartId, ProductId = ProductId, Quantity = Quantity };
-                connection.Execute(insert, parameters);
+                var query = "select * from Cart where CartId = @CartId and ProductId = @ProductId";
+                var queryParameter = new { CartId = CartId, ProductId = ProductId };
+                var queryStatement = connection.QuerySingleOrDefault<CartViewModel>(query, queryParameter);
+
+                if (queryStatement != null)
+                {
+                    var update = "update Cart set Quantity = Quantity + @Quantity where ProductId = @ProductId";
+                    var updateParameter = new { Quantity = Quantity, ProductId = ProductId };
+                    connection.Execute(update, updateParameter);
+                } else
+                {
+                    var insert = "insert into Cart (CartId, ProductId, Quantity) values (@CartId, @ProductId, @Quantity)";
+                    var parameters = new { CartId = CartId, ProductId = ProductId, Quantity = Quantity };
+                    connection.Execute(insert, parameters);
+                }
+
             }
 
             return RedirectToAction("Index", "Products");
+        }
+
+        [HttpPost]
+        public ActionResult ChangeQuantityInCart (int ProductId, int Quantity)
+        {
+            using (var connection = new SqlConnection(this.connectionString))
+            {
+                var update = "update Cart set Quantity = @Quantity where ProductId = @ProductId";
+                var updateParameter = new { Quantity = Quantity, ProductId = ProductId };
+                connection.Execute(update, updateParameter);
+            }
+            return RedirectToAction("ShowCart");
+        }
+
+        [HttpPost]
+        public ActionResult DeleteCart(int ProductId)
+        {
+            using (var connection = new SqlConnection(this.connectionString))
+            {
+                var delete = "delete from Cart where ProductId = @ProductId";
+                var deleteParameter = new { ProductId = ProductId };
+                connection.Execute(delete, deleteParameter);
+            }
+            return RedirectToAction("ShowCart");
         }
     }
 }
