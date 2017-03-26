3a0732dcd386433c6ba02178bdff765c9bc22429
Maria Gustafsson Niså <maria.gnisa@gmail.com>
2017-03-26 12:22:13 +0200

    added admin auth

--- e-commerce/Controllers/AdminController.cs
+++ e-commerce/Controllers/AdminController.cs
@@ -8,6 +8,7 @@ using System.Configuration;
 using System.Data.SqlClient;
 using Dapper;
 
+
 namespace e_commerce.Controllers
 {
     
@@ -16,8 +17,30 @@ namespace e_commerce.Controllers
         private readonly string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
 
         // GET: Admin
+        [HttpGet]
         public ActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string Username, string Password)
+        {
+            using (var connection = new SqlConnection(this.connectionString))
+            {
+                var query = "select * from Admin where Username = @username and Password = @password";
+                var parameters = new { username = Username, password = Password };
+                var authAdmin = connection.QuerySingleOrDefault(query, parameters);
+
+                if (authAdmin != null)
+                {
+                    Session["admin"] = authAdmin;
+
+                    return RedirectToAction("AddProduct");
+                }
+              
+            }
+            System.Web.HttpContext.Current.Response.Write("<script>alert('Wrong username or password. Please try again.');</script>");
             return View();
         }
 
@@ -28,15 +51,26 @@ namespace e_commerce.Controllers
         }
 
         [HttpPost]
-        public ActionResult AddProduct(ProductsViewModel model)
+        public ActionResult AddProduct(ProductsViewModel model, HttpPostedFileBase file)
         {
             using (var connection = new SqlConnection(this.connectionString))
             {
-                var insert = "insert into Products (ProductName, ProductDescription, ProductPrice, ImgPath) values (@Name, @Description, @Price, @Path)";
-                var parameters = new { Name = model.ProductName, Description = model.ProductDescription, Price = model.ProductPrice, Path = model.ImgPath };
-                connection.Execute(insert, parameters);
+                //verify that a file is selected
+                if(file != null && file.ContentLength > 0)
+                {
+                    //extract the filename and adds the current timestamp
+                    var fileName = Environment.TickCount + "_" + file.FileName;
+                    //Save the file to given path
+                    file.SaveAs(Server.MapPath("~\\Content\\img\\") + fileName);
+
+                    var insert = "insert into Products (ProductName, ProductDescription, ProductPrice, ImgPath) values (@Name, @Description, @Price, @Path)";
+                    var parameters = new { Name = model.ProductName, Description = model.ProductDescription, Price = model.ProductPrice, Path = fileName };
+                    connection.Execute(insert, parameters);
+                }
             }
                 return RedirectToAction("AddProduct");
         }
+
+        
     }
 }
