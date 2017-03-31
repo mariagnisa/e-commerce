cfae197d6d97caab1b2aff3ba77691081b61f8d8
Maria Gustafsson Niså <maria.gnisa@gmail.com>
2017-03-31 11:29:08 +0200

    added properties?

--- e-commerce/Models/CheckoutViewModel.cs
+++ e-commerce/Models/CheckoutViewModel.cs
@@ -5,7 +5,32 @@ using System.Web;
 
 namespace e_commerce.Models
 {
+
     public class CheckoutViewModel
     {
+        //order info
+        public string OrderId { get; set; }
+        public int Orderstatus { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string CartId { get; set; }
+
+        //order product info
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public int ProductPrice { get; set; }
+        public int Price { get; set; }
+        
+        //customer info
+        public int CustomerId { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+
     }
 }
