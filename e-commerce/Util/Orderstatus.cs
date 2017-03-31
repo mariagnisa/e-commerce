7ebe608862d7bced622d733f952972d7c65c8875
Maria Gustafsson Niså <maria.gnisa@gmail.com>
2017-03-31 11:33:56 +0200

    added folder

---
+++ e-commerce/Util/Orderstatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace e_commerce.Util
+{
+    public enum Orderstatus
+    {
+            UnderProgress = 1,
+            Succes = 2,
+            Cancelled = 3
+    }
+}
