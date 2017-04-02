a41ccdd14e6c4382cda1d44eba9dd2097529ed48
Maria Gustafsson Niså <maria.gnisa@gmail.com>
2017-04-02 17:05:01 +0200

    fixed ajax to work properly

--- e-commerce/Scripts/cameraObscura.js
+++ e-commerce/Scripts/cameraObscura.js
@@ -9,28 +9,42 @@ function Post(data, url) {
     })
     .then(function (response) { return response.json(); })
     .then(function (result) {
-        let messageBox = document.querySelector('.messageBox');
-        messageBox.innerHTML = result.message;
-        messageBox.style.display = "block";
-        
+      
         if (result.succes) {
-            messageBox.classList.add('alert');
-            messageBox.classList.add('alert-success');
-            setTimeout(function () {
-                messageBox.classList.remove('alert');
-                messageBox.classList.remove('alert-success');
-                messageBox.style.display = "none";
-            }, 3000);
+            showSuccess(result.message);
+            updateCart();
         } else {
-            messageBox.classList.add('alert');
-            messageBox.classList.add('alert-danger');
-            setTimeout(function () {
-                messageBox.classList.remove('alert');
-                messageBox.classList.remove('alert-danger');
-                messageBox.style.display = "none";
-            }, 3000);
+            showError(result.message);
         }
     })
+}
+
+function showError(message) {
+    let messageBox = document.querySelector('.messageBox');
+    messageBox.innerHTML = message;
+    messageBox.style.display = "block";
+
+    messageBox.classList.add('alert');
+    messageBox.classList.add('alert-danger');
+    setTimeout(function () {
+        messageBox.classList.remove('alert');
+        messageBox.classList.remove('alert-danger');
+        messageBox.style.display = "none";
+    }, 3000);
+}
+
+function showSuccess(message) {
+    let messageBox = document.querySelector('.messageBox');
+    messageBox.innerHTML = message;
+    messageBox.style.display = "block";
+
+    messageBox.classList.add('alert');
+    messageBox.classList.add('alert-success');
+    setTimeout(function () {
+        messageBox.classList.remove('alert');
+        messageBox.classList.remove('alert-success');
+        messageBox.style.display = "none";
+    }, 3000);
 }
 
 let addButtons = document.querySelectorAll('.addProduct');
@@ -39,13 +53,33 @@ addButtons.forEach(function (button) {
     button.addEventListener('click', function (event) {
         event.preventDefault();
         let formData = new FormData;
-        formData.append('ProductId', button.parentElement.querySelector('input[name=ProductId]').value);
-        formData.append('Quantity', button.parentElement.querySelector('input[name=Quantity]').value);
+
+        var productId = button.parentElement.querySelector('input[name=ProductId]').value;
+        var qty = button.parentElement.querySelector('input[name=Quantity]').value;
+
+        if ((productId != undefined && productId.length > 0) && (qty != undefined && qty > 0)) {
+            formData.append('ProductId', productId);
+            formData.append('Quantity', qty);
         
-        Post(formData, '/Cart/AddItemToCart');
+            Post(formData, '/Cart/AddItemToCart');
+        } else {
+            showError("Please provide a valid quantity.");
+        }
 
     })
 })
+
+function updateCart() {
+    let cartContainer = document.querySelector(".cart-container");
+    fetch('/cart/Index', { credentials: "same-origin" })
+    .then(function (response) { return response.text(); })
+    .then(function (result) {
+        
+        cartContainer.innerHTML = result;
+        updateQuantity();
+        deleteProduct();
+    })
+}
 
 function updateQuantity() {
     let updateButtons = document.querySelectorAll('.updateQuantity');
@@ -53,11 +87,21 @@ function updateQuantity() {
         button.addEventListener('click', function (event) {
             event.preventDefault();
             let formData = new FormData;
-            formData.append('ProductId', button.parentElement.querySelector('input[name=ProductId]').value);
-            formData.append('CartId', button.parentElement.querySelector('input[name=CartId]').value);
-            formData.append('Quantity', button.parentElement.querySelector('input[name=Quantity]').value);
-            
-            Post(formData, '/Cart/ChangeQuantityInCart');
+
+            var productId = button.parentElement.querySelector('input[name=ProductId]').value;
+            var CartId = button.parentElement.querySelector('input[name=CartId]').value;
+            var qty = button.parentElement.querySelector('input[name=Quantity]').value;
+
+            if ((productId != undefined && productId.length > 0) && (CartId != undefined && CartId.length > 0) && (qty != undefined && qty > 0)) {
+                formData.append('ProductId', productId);
+                formData.append('CartId', CartId);
+                formData.append('Quantity', qty);
+
+                Post(formData, '/Cart/ChangeQuantityInCart');
+            } else {
+                showError("Please provide a valid quantity.");
+            }
+
         })
     })
 }
@@ -70,14 +114,20 @@ function deleteProduct() {
         button.addEventListener('click', function (event) {
             event.preventDefault();
             let formData = new FormData;
-            formData.append('ProductId', button.parentElement.querySelector('input[name=ProductId]').value);
-            formData.append('CartId', button.parentElement.querySelector('input[name=CartId]').value);
 
-            Post(formData, '/Cart/DeleteFromCart');
-            setTimeout(function () {
-                location.reload();
-            }, 3000);
-           
+            var productId = button.parentElement.querySelector('input[name=ProductId]').value;
+            var CartId = button.parentElement.querySelector('input[name=CartId]').value;
+
+            if ( (productId != undefined && productId.length > 0) && (CartId != undefined && CartId.length > 0) ) {
+                formData.append('ProductId', productId);
+                formData.append('CartId', CartId);
+
+                Post(formData, '/Cart/DeleteFromCart');
+             
+            } else {
+                showError("Please select a product from your cart.");
+            }
+ 
         })
     })
 }
